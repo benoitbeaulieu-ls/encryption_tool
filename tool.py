5d78459fefbce6c13ed4bf85981a1d4f1ced0a8f
benoitbeaulieu-ls <benoit.beaulieub@gmail.com>
2020-12-13 21:39:55 -0500

    Started key creation

--- tool.py
+++ tool.py
@@ -1,1 +1,29 @@
-print('hello')
+import gnupg
+import os
+import platform
+import subprocess
+
+
+os_type = (platform.system())
+
+def key_creation():
+    key_store=input('Where would you like to store your key?')
+
+    gpg=gnupg.GPG(gnupghome='~')
+    gpg.encoding = 'utf-8'
+    key_info = gpg.gen_key_input()
+
+
+
+
+
+if os_type == 'Linux':
+    print('Detected Linux Operating System')
+    print('#########################################')
+    key_creation()
+
+
+
+    
+
+
